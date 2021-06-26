28f59ab8f9a4a03dcefb486f96b7a7546a0677cb
Qphuc <tqp1610@gmail.com>
2021-06-26 20:42:19 +0700

    css object

--- src/Styling/Styling.js
+++ src/Styling/Styling.js
@@ -5,10 +5,18 @@ import Child from './Child'
 
 export default class Styling extends Component {
     render() {
+        //Luu y viet duoi dang thuoc tinh document.getElementById('id').style.backgroundColor
+        const styleText = {
+            color: 'pink',
+            padding: '15px',
+            backgroundColor: 'black',
+        }
+
         return (
             <div>
                 <p className="txt">Hello</p>
                 <span className={style.txtStyle}>Hello module</span>
+                <p style={styleText}>Hello 123123</p>
                 <Child />
             </div>
         )
