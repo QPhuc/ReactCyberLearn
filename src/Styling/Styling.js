04f7e865264420ffcc9b06cd13aeb4b2b532febb
Qphuc <tqp1610@gmail.com>
2021-06-26 20:36:51 +0700

    module css

--- src/Styling/Styling.js
+++ src/Styling/Styling.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import './Styling.css'
-import style from 'S'
+import './Styling.css';
+import style from './Styling.module.css';
 import Child from './Child'
 
 export default class Styling extends Component {
@@ -8,6 +8,7 @@ export default class Styling extends Component {
         return (
             <div>
                 <p className="txt">Hello</p>
+                <span className={style.txtStyle}>Hello module</span>
                 <Child />
             </div>
         )
