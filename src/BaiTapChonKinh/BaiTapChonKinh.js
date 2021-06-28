a8897c7f9cd24ddb7a1d2e49dd01940fab9b5bf3
Qphuc <tqp1610@gmail.com>
2021-06-28 21:09:48 +0700

    state

--- src/BaiTapChonKinh/BaiTapChonKinh.js
+++ src/BaiTapChonKinh/BaiTapChonKinh.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 
 export default class BaiTapChonKinh extends Component {
 
+    state = {
+        imgGlasses: './glassesImage/g1.jpg'
+    }
+
     renderGlasses = () => {
 
     }
