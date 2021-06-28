7f22a35adb13861d74dc211c9a4f7a62cac2e654
Qphuc <tqp1610@gmail.com>
2021-06-28 20:58:00 +0700

    row

--- src/BaiTapChonKinh/BaiTapChonKinh.js
+++ src/BaiTapChonKinh/BaiTapChonKinh.js
@@ -5,7 +5,16 @@ export default class BaiTapChonKinh extends Component {
         return (
             <div style={{backgroundImage:'url(./glassesImage/background.jpg)', minHeight:'700px'}}>
                 <div style={{ backgroundColor: 'rgba(0,0,0,0.3)', minHeight:'700px'}}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6">
 
+                            </div>
+                            <div className="col-6">
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
