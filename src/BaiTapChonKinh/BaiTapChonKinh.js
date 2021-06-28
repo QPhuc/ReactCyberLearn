4f2ee37abf9ee422a25db84629e4246945c3a9fa
Qphuc <tqp1610@gmail.com>
2021-06-28 20:55:34 +0700

    background

--- src/BaiTapChonKinh/BaiTapChonKinh.js
+++ src/BaiTapChonKinh/BaiTapChonKinh.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class BaiTapChonKinh extends Component {
     render() {
         return (
-            <div>
-                
+            <div style={{backgroundImage:'url(./glassesImage/background.jpg)', minHeight:'700px'}}>
+                <div>
+                    
+                </div>
             </div>
         )
     }
