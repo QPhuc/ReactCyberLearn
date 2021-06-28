314811ecc321ff94cee05dedcc417bdffbf2ba88
Qphuc <tqp1610@gmail.com>
2021-06-28 20:56:25 +0700

    BackgroundColor

--- src/BaiTapChonKinh/BaiTapChonKinh.js
+++ src/BaiTapChonKinh/BaiTapChonKinh.js
@@ -4,8 +4,8 @@ export default class BaiTapChonKinh extends Component {
     render() {
         return (
             <div style={{backgroundImage:'url(./glassesImage/background.jpg)', minHeight:'700px'}}>
-                <div>
-                    
+                <div style={{ backgroundColor: 'rgba(0,0,0,0.3)', minHeight:'700px'}}>
+
                 </div>
             </div>
         )
